fe035305b86a8a869272c42e93e4f9d9ac10c080
BhuvaneshwaranKannan <bhuvaneshwarkannan27@gmail.com>
2026-06-03 22:51:02 +0530

    Added Home Page Loading

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,25 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './Sidebar'
 import Feed from './Feed'
 import SuggestionList from './SuggestionList';
 import Messages from './Messages';
 
+import instaLogo from './assets/instaLogo.png'
+
+import { useEffect } from "react";
+import useFetch from './useFetch';
 
 function Home() {
 
+    const [homePage, sethomePage] = useState(false)
+
+    useEffect(() => {
+        if (!homePage) {
+            const timer = setTimeout(() => {
+                sethomePage(true);
+            }, 2000);
+
+            return () => clearTimeout(timer); // cleanup
+        }
+    }, [homePage]);
+
     return (
         <>
-            <div className='d-flex vh-100'>
+            {
+                homePage ? (
+                    <div className='d-flex vh-100'>
 
-                <div className='w-14'> <Sidebar /> </div>
+                        <div className='w-14'> <Sidebar /> </div>
 
-                <div className='w-50'> <Feed /> </div>
+                        <div className='w-50'> <Feed /> </div>
 
-                <div className='w-23'> <SuggestionList /> </div>
+                        <div className='w-23'> <SuggestionList /> </div>
 
-                <div className='w-18'> <Messages /></div>
+                        <div className='w-18'> <Messages /></div>
 
-            </div>
+                    </div>
+                ) : (
+
+                    <div className="homeLoadPage">
+
+                        <div className="loadingPageInstaIcon">
+                            <img src={instaLogo} alt="" className='loadingPageIcon' />
+
+                        </div>
+                        <div className="loadingPageCaption">
+                            <div className='lpcFrom'>
+                                <h6>from</h6>
+                            </div>
+                            <div>
+                                <h3 className='colorQuote'>V I S H I</h3>
+                            </div>
+                        </div>
+
+                    </div>
+                )
+            }
+
         </>
 
     )
